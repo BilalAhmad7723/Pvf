3aa3393cd32f274380ca76d091b6b9f2360c6bec
Bilal Ahmad <bilalahmad7723@gmail.com>
2022-09-13 16:14:29 +0500

    update Backend

--- routes/Candidate/candidate_route.js
+++ routes/Candidate/candidate_route.js
@@ -18,8 +18,6 @@ router.route('/get_candidates').get((req, res, next) => {
 router.route('/add-candidates').post((req, res, next) => {
   console.log("Add Candidate1"+ req.body.edu);
   canidateSchema.create(req.body, (error, data) => {
-    console.log("Error Add Canidate:"+ error);
-    console.log("Data Add Canidate:" + data);
     if (error) {
       return next(error)
     } else {
